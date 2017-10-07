3e09b1ef467dee9e0af22219b79187d4fdad30f7
Sushmit <er.sushmit@gmail.com>
2017-10-07 22:22:52 +0530

    sg edge backgorund fixed

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,5 @@
+var particlesJS = require('./particle.js');
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', '/particlesjs-config.json', function() {
   console.log('callback - particles-js config loaded');
