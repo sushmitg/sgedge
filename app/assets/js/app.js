ec720dec2941b995a7442e7da59fc4076625ece6
Sushmit <er.sushmit@gmail.com>
2017-10-09 06:25:03 +0530

    added webpack and animation

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,30 @@
-var particlesJS = require('./particle.js');
+require('../../../node_modules/waypoints/lib/noframework.waypoints.min');
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/particlesjs-config.json', function() {
-  console.log('callback - particles-js config loaded');
+[].forEach.call(document.querySelectorAll('.on-scroll-reveal'),
+  function(el){
+    el.className += ' animated';
+    var waypoint = new Waypoint({
+      element: el,
+      handler: function() {
+        this.element.className += ' fadeInUp';
+      },
+      offset: '80%'
+    })
+  }
+);
+
+
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+var menu = document.querySelector(".menu");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  if (menu.style.display === "none" || menu.style.display === "") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,9 +14,16 @@ gulp.task('watch', function(){
   watch('./app/assets/css/**/*.css', function(){
     gulp.start('cssInject');
   });
+  watch('./app/assets/js/**/*.js', function(){
+    gulp.start('scriptsRefresh');
+  })
 });
 
 gulp.task('cssInject', ['styles'],function(){
   gulp.src('./app/temp/css/styles.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function () {
+  browserSync.reload();
 });
