12d143d07b8747238dd0d0b060b5d7335e8a243c
sumit laptop <er.sushmit@gmail.com>
2017-10-12 23:19:49 +0530

    Footer Parallax + mobile nav fix

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,96 +1,38 @@
 require('../../../node_modules/waypoints/lib/noframework.waypoints.min');
 
-
 function attach(element, listener, ev, tf) {
-
-  if (element.attachEvent) {
-
+  if (element.attachEvent) { //if it's <= IE8
     element.attachEvent("on" + listener, ev);
-
   } else {
-
     element.addEventListener(listener, ev, tf);
-
   }
-
 }
 
 function fadeOut(element, startLevel, endLevel, duration, callback) {
-
   var fOInt;
-
   op = startLevel;
-
   fOInt = setInterval(function() {
-
     if (op <= endLevel) {
-
       element.style.opacity = endLevel;
       element.style.filter = "alpha(opacity = " + endLevel + ")";
-
       clearInterval(fOInt);
-
       if (typeof callback == 'function') callback(true);
-
     } else {
-
       op -= 0.1;
       element.style.opacity = op;
       element.style.filter = "alpha(opacity = " + op * 100 + ")";
-
     }
-
   }, duration);
-
 }
-var loader = document.querySelector('.loader');
 
+var loader = document.querySelector('.loader');
 attach(window, 'load', function() {
   fadeOut(loader, 1, 0, 50, function(cb) {
     loader.style.display = 'none';
   });
-
   var heroText = document.querySelector('.large-hero__text-content');
-
   heroText.classList += ' fadeInUp--Hero';
-
 }, false);
-
-// function fadeOut(el){
-//   el.style.opacity = 1;
-//
-//   (function fade() {
-//     if ((el.style.opacity -= .1) < 0) {
-//       el.style.display = "none";
-//     } else {
-//       requestAnimationFrame(fade);
-//     }
-//   })();
-// };
-//
-// window.addEventListener('load', fadeOut(el));
-
-// // Show loader while page loads....
-// function onReady(callback) {
-//     var intervalID = window.setInterval(checkReady, 2000);
-//     function checkReady() {
-//         if (document.getElementsByTagName('body')[0] !== undefined) {
-//             window.clearInterval(intervalID);
-//             callback.call(this);
-//         }
-//     }
-// }
-//
-// function show(el, value) {
-//     document.querySelector(el).style.display = value ? 'block' : 'none';
-// }
-//
-// onReady(function () {
-//     show('.loader', false);
-//     show('body', true);
-// });
-
-
 
 // Shrink Logo on scroll
 // Look for Logo
@@ -107,25 +49,26 @@ document.addEventListener('scroll', function() {
   }
 });
 
-
-
 // Reveal on Scroll Animation -- Waypoints.js
 [].forEach.call(document.querySelectorAll('.on-scroll-reveal'), function(el) {
+  var animationClass = el.dataset.animation;
+  var offset = el.dataset.offset;
   // Add class animated frome Animate.css and
+  el.classList.add('animated');
   // opacity to 0 to hide element initially...
-  el.className += ' animated';
-  el.style.opacity = 0;
+  el.style.opacity = '0';
   // adding on scroll trigger using waypoints.js
   var waypoint = new Waypoint({
     element: el,
     handler: function() {
-      this.element.className += ' fadeInUp';
+      this.element.classList.add(animationClass);
+      el.style.opacity = '1';
     },
-    offset: '80%'
+    offset: offset
   })
 });
 
-
+// Mobile hamburger Click functionality
 // Look for .hamburger
 var hamburger = document.querySelector(".hamburger");
 var menu = document.querySelector(".menu");
@@ -140,3 +83,29 @@ hamburger.addEventListener("click", function() {
     menu.style.display = "none";
   }
 });
+
+// Hide Menu when clicked on anchor
+menu.addEventListener("click", function (e) {
+  if (e.target.nodeName = "A") {
+    menu.style.display = "none"
+    hamburger.classList.toggle("is-active");
+  }
+});
+
+//Contact Us section Bottom Margin equals to Footer height for Reveal Parallax effect.
+function getStyle(oElm, strCssRule) {
+ 	var strValue = "";
+ 	if(document.defaultView && document.defaultView.getComputedStyle){
+ 		strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+ 	}
+ 	else if(oElm.currentStyle){
+ 		strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1){
+ 			return p1.toUpperCase();
+ 		});
+ 		strValue = oElm.currentStyle[strCssRule];
+ 	}
+ 	return strValue;
+}
+
+var footerHeight = getStyle(document.querySelector('footer'), "height");
+document.querySelector('#contact-us').style.marginBottom =  footerHeight;
