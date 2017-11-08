8d1fc30769e7ca261a268d6b26d17c394760c4ff
Sushmit <er.sushmit@gmail.com>
2017-11-08 06:40:25 +0530

    distribution ready

---
+++ gulp/tasks/build.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify');
+
+
+gulp.task('deleteDistFolder', function (){
+  return del('./dist');
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function () {
+  return gulp.src('./app/assets/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      multipass: true
+    }))
+    .pipe(gulp.dest('./dist/assets/images'));
+});
+
+gulp.task('usemin', ['deleteDistFolder'], function(){
+  return gulp.src('./app/*.html')
+    .pipe(usemin({
+      css: [function(){return rev()}, function(){return cssnano()}],
+      js: [function(){return rev()}, function(){return uglify()}]
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['deleteDistFolder','optimizeImages', 'usemin']);
